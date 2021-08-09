f8d3a5a2ed2471196c98ae24e9c86ff6ce2b820a
Vlad <vlad-skyns@yandex.ru>
2021-08-09 13:56:57 +0300

    migrations

---
+++ database/migrations/2021_08_09_093754_create_nw_items_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNwItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nw_professions', function (Blueprint $table) {
+            $table->integer('id')->unique();
+            $table->integer('profession')->unique();
+        });
+
+        Schema::create('nw_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->string('img')->default('https://online.forda.ru/img/square.gif');
+            $table->integer('tier')->nullable();
+            $table->boolean('is_craftable');
+            $table->integer('profession_id')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('profession_id')->references('id')->on('nw_professions')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nw_professions');
+        Schema::dropIfExists('nw_items');
+    }
+}
